d35e3f10ede83a872a87ff526af9a8ee406e987d
Rafael Felipe <rafaelfelipeac@gmail.com>
2017-09-29 00:07:56 -0300

    Add the model folder.

---
+++ app/src/main/java/br/com/rafaelfelipeac/keepcoin/model/Goal.kt
@@ -0,0 +1,10 @@
+package br.com.rafaelfelipeac.keepcoin.model
+
+/**
+ * Created by Rafael Felipe on 28/09/2017.
+ */
+
+class Goal() {
+    var name: String = ""
+    var value: Float = 0.0f
+}
---
+++ app/src/main/java/br/com/rafaelfelipeac/keepcoin/model/Income.kt
@@ -0,0 +1,9 @@
+package br.com.rafaelfelipeac.keepcoin.model
+
+/**
+ * Created by Rafael Felipe on 28/09/2017.
+ */
+
+class Income(){
+    var value: Float = 0.0f
+}
